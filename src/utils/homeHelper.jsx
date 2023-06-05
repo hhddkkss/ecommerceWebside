1514ed164f3ffcbfc93673094403f5d9f5ee800a
hhddkkss <116135735+hhddkkss@users.noreply.github.com>
2023-06-06 02:58:39 +0800

    新增 fetchApi

--- src/utils/homeHelper.jsx
+++ src/utils/homeHelper.jsx
@@ -1,8 +1,18 @@
 import axios from 'axios'
 
+//新鮮事用
 export const fetchNews = async () => {
   return await axios
     .get('http://localhost:3003/home_page/home_articles')
+    .then((res) => res.data)
+    .catch((e) => {
+      throw new Error(e)
+    })
+}
+//熱門商品用
+export const fetchPopularProduct = async () => {
+  return await axios
+    .get('http://localhost:3003/home_page/recommend_product')
     .then((res) => res.data)
     .catch((e) => {
       throw new Error(e)
