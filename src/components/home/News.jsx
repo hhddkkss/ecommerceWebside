af002d9cf938635cbe30d29390db6ce5da0293f0
hhddkkss <116135735+hhddkkss@users.noreply.github.com>
2023-06-11 13:56:50 +0800

    修改news卡片樣式

--- src/components/home/News.jsx
+++ src/components/home/News.jsx
@@ -34,95 +34,102 @@ const News = () => {
     <Box
       sx={{
         p: '2rem',
-        background: '#e0e0de;',
+        maxWidth: '1200px',
+        margin: 'auto',
       }}
     >
       <Typography
         variant="h4"
         color="text.secondary"
-        sx={{ fontFamily: 'var(--jfopenhuninn)' }}
+        textAlign="center"
+        sx={{ fontFamily: 'var(--jfopenhuninn)', mb: 4 }}
       >
-        - 新鮮事
+        新鮮事
       </Typography>
-      <Grid
-        container
-        spacing={10}
-        sx={{ p: { md: 10, xs: 4 } }}
-        align-items="stretch"
-      >
-        {newsData.map((item) => (
-          <Grid item key={item.article_id} md={4} xs={12}>
-            <Card
-              sx={{
-                height: '100%',
-                '&:hover > div > img': {
-                  transform: 'scale(1.1)',
-                  filter: 'blur(1px)',
-                },
-              }}
-              raised={true}
-            >
-              <Box sx={{ overflow: 'hidden' }}>
-                <CardMedia
-                  sx={{
-                    height: 200,
-                    objectFit: 'cover',
-                    transition: 'transform 0.5s ease-in',
-                  }}
-                  component="img"
-                  image={'/images/article/' + item.article_pic_main}
-                />
-              </Box>
-              <CardContent
+      <Box sx={{ maxWidth: '1200px', margin: '0 auto' }}>
+        <Grid container spacing={2} alignItems="stretch">
+          {newsData.map((item) => (
+            <Grid item key={item.article_id} sm={4} xs={12}>
+              <Card
                 sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
+                  height: '100%',
+                  transition: 'backgroundColor 0.3s ease-in',
+                  '&:hover': {
+                    backgroundColor: '#ddd',
+                  },
+                  '&:hover > div > img': {
+                    transform: 'scale(1.03)',
+                  },
                 }}
+                raised={true}
               >
-                <Typography
-                  gutterBottom
-                  variant="h5"
-                  component="div"
-                  style={{
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    display: '-webkit-box',
-                    WebkitLineClamp: '1',
-                    WebkitBoxOrient: 'vertical',
-                    marginBottom: '2rem',
+                <Box sx={{ overflow: 'hidden' }}>
+                  <CardMedia
+                    sx={{
+                      height: '300px',
+                      objectFit: 'cover',
+                      transition: 'transform 0.5s ease-in',
+                      verticalAlign: 'middle',
+                      clipPath:
+                        'polygon(50% 0%, 100% 0, 100% 50%, 50% 100%, 50% 100%, 0 50%, 0 0)',
+                    }}
+                    component="img"
+                    image={'/images/article/' + item.article_pic_main}
+                  />
+                </Box>
+                <CardContent
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
                   }}
                 >
-                  {item.title}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                  style={{
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    display: '-webkit-box',
-                    WebkitLineClamp: '5',
-                    WebkitBoxOrient: 'vertical',
-                    marginBottom: '2rem',
-                  }}
-                >
-                  {item.content_1}
-                </Typography>
-                <Typography
-                  variant="h6"
-                  color="text.primary"
-                  sx={{ textAlign: 'right' }}
-                >
-                  {dayjs(item.created_at).format('YYYY-MM-DD')}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    style={{
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                      display: '-webkit-box',
+                      WebkitLineClamp: '1',
+                      WebkitBoxOrient: 'vertical',
+                      marginBottom: '2rem',
+                    }}
+                  >
+                    {item.title}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    style={{
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                      display: '-webkit-box',
+                      WebkitLineClamp: '5',
+                      WebkitBoxOrient: 'vertical',
+                      marginBottom: '2rem',
+                    }}
+                  >
+                    {item.content_1}
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    color="text.primary"
+                    sx={{ textAlign: 'right' }}
+                  >
+                    {dayjs(item.created_at).format('YYYY-MM-DD')}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" variant="contained" color={'primary'}>
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Box>
   )
 }
