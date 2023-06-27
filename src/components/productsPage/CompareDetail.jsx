0552c44680dc447d8498c1d6eadd02e80a4106eb
hhddkkss <116135735+hhddkkss@users.noreply.github.com>
2023-06-28 01:06:25 +0800

    css微調

--- src/components/productsPage/CompareDetail.jsx
+++ src/components/productsPage/CompareDetail.jsx
@@ -110,7 +110,7 @@ const CompareDetail = ({
                 sx={{
                   width: '50px',
                   height: '50px',
-                  color: theme.normal.textColorWhite,
+                  color: '#fff',
                 }}
               >
                 <CloseIcon />
@@ -280,7 +280,7 @@ const CompareDetail = ({
                     }}
                     onClick={() => handleDeleteFromComparing(v.product_id)}
                   >
-                    <RemoveIcon />
+                    <RemoveIcon color="error" />
                   </IconButton>
                   <Box>
                     <Box
